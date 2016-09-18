a359b944ae0980eb0a3b848669fd82a85ae3fccb
James Henstridge <james@jamesh.id.au>
2016-09-19 07:12:40 +0800

    Minor indentation fix.

--- src/item_id.cpp
+++ src/item_id.cpp
@@ -7,7 +7,8 @@ using namespace unity::storage::provider;
 
 static const auto URL_FORMAT = QUrl::FullyEncoded | QUrl::NormalizePathSegments;
 
-QUrl id_to_url(string const& item_id, QUrl const& base_url) {
+QUrl id_to_url(string const& item_id, QUrl const& base_url)
+{
     QUrl item_url(QString::fromStdString(item_id), QUrl::StrictMode);
     if (!item_url.isValid())
     {
@@ -26,7 +27,8 @@ QUrl id_to_url(string const& item_id, QUrl const& base_url) {
     return item_url;
 }
 
-string url_to_id(QUrl const& item_url, QUrl const& base_url) {
+string url_to_id(QUrl const& item_url, QUrl const& base_url)
+{
     QByteArray base_url_bytes = base_url.toEncoded(URL_FORMAT);
     QByteArray item_url_bytes = item_url.toEncoded(URL_FORMAT);
     if (!item_url_bytes.startsWith(base_url_bytes))
