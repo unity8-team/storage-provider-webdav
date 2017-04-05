9b1c8014557e2a30bf46a244307bce9bd1c776ff
James Henstridge <james@jamesh.id.au>
2017-04-05 12:19:53 +0800

    Build both Nextcloud and OwnCloud versions of the provider.

---
+++ src/nextcloud.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: James Henstridge <james.henstridge@canonical.com>
+ */
+
+#include <unity/storage/provider/Server.h>
+
+#include "NextcloudProvider.h"
+
+using namespace std;
+using namespace unity::storage::provider;
+
+int main(int argc, char **argv)
+{
+    string const bus_name = "com.canonical.StorageFramework.Provider.Nextcloud";
+    string const account_service_id = "storage-provider-nextcloud";
+
+    Server<NextcloudProvider> server(bus_name, account_service_id);
+    server.init(argc, argv);
+    server.run();
+    return 0;
+}
